8bd6b63a58c9854786546c80eb165ec884a6cdf3
ted.bashor <ted.bashor@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2008-02-06 08:31:54 +0000

    Implement the following dirty/save behavior  * all mule config model changes in the Overview or Services page and their associated Properties view cause the file to be marked dirty  * diagram edits in the Services page will NOT cause the file to be marked as dirty  * on user save, both the config model and diagram model (as needed) will be saved  * on file close, the diagram model will be silently saved as needed.  normal prompting will occur if the config model is dirty.
    To make this work, I had to unhook Jesper's partial two-way editing implementation, because it was dirtying the source editor on some config model changes but not others.

--- src/main/plugins/org.mule.ide.config.editor.services/src/org/mule/ide/config/editor/services/part/CoreDiagramEditor.java
+++ src/main/plugins/org.mule.ide.config.editor.services/src/org/mule/ide/config/editor/services/part/CoreDiagramEditor.java
@@ -150,7 +150,23 @@ public class CoreDiagramEditor extends DiagramDocumentEditor implements
 	public void gotoMarker(IMarker marker) {
 		MarkerNavigationService.getInstance().gotoMarker(this, marker);
 	}
+	
+	public boolean isDiagramDirty() {
+		return isDirty();
+	}
 
+	protected void firePropertyChange(int property) {
+		if (property == PROP_DIRTY  && isDirty()) {
+			// Intercept diagram model dirty events
+			// Diagram model will be saved on config model saves,
+			// or on document close if needed.
+			//System.out.println("CoreDiagramEditor.firePropertyChange : eating PROP_DIRTY event");
+		} else {
+			super.firePropertyChange(property);
+		}
+		
+	}
+	
 	/**
 	 * @generated
 	 */

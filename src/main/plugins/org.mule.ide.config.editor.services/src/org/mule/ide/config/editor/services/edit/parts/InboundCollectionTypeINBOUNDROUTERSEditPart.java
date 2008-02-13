3415069525ff9b5af51c7ec78bdf8781929c22f3
ted.bashor <ted.bashor@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2008-02-13 05:50:23 +0000

    Sort children of the inbound and async collection edit parts such that inbound endpoints are listed before routers.

--- src/main/plugins/org.mule.ide.config.editor.services/src/org/mule/ide/config/editor/services/edit/parts/InboundCollectionTypeINBOUNDROUTERSEditPart.java
+++ src/main/plugins/org.mule.ide.config.editor.services/src/org/mule/ide/config/editor/services/edit/parts/InboundCollectionTypeINBOUNDROUTERSEditPart.java
@@ -1,7 +1,12 @@
 package org.mule.ide.config.editor.services.edit.parts;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ListCompartmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
@@ -10,6 +15,7 @@ import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableCompartmentEditPolicy;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.View;
+import org.mule.ide.config.core.AbstractInboundEndpointType;
 import org.mule.ide.config.core.InboundCollectionType;
 import org.mule.ide.config.editor.services.edit.policies.InboundCollectionTypeINBOUNDROUTERSCanonicalEditPolicy;
 import org.mule.ide.config.editor.services.edit.policies.InboundCollectionTypeINBOUNDROUTERSItemSemanticEditPolicy;
@@ -73,6 +79,30 @@ public class InboundCollectionTypeINBOUNDROUTERSEditPart extends
 		// super.setRatio(ratio); 
 	}
 
+	@Override
+	//TODO  Probably should figure out how to use the GMF Style mechanism instead of overriding.
+	protected List getSortedChildren() {
+		EList allChildren;
+        Object model = getModel();
+        if(model!=null && model instanceof View){
+        	allChildren = ((View)model).getVisibleChildren();
+        } else {
+        	return Collections.EMPTY_LIST;
+        }
+		
+		List<View> sorted = new ArrayList<View>(allChildren.size());
+		int i = 0;
+		for (int j = 0; j < allChildren.size(); j++) {
+			View view = (View) allChildren.get(j);
+			if (view.getElement() instanceof AbstractInboundEndpointType) {
+				sorted.add(i++, view);
+			} else {
+				sorted.add(view);
+			}
+		}
+		return sorted;
+	}
+	
 	@Override
 	protected void handleNotificationEvent(Notification notification) {
 		if (notification.getNotifier() instanceof InboundCollectionType) {

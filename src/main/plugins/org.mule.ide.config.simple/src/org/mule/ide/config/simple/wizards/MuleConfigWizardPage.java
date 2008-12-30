12eeb27299dace3f2846d3fe4d52c9faa816e9b4
dirk <dirk@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2008-12-30 10:04:03 +0000

    New plugin for a simple config file wizard that helps creating a new config file

---
+++ src/main/plugins/org.mule.ide.config.simple/src/org/mule/ide/config/simple/wizards/MuleConfigWizardPage.java
@@ -0,0 +1,236 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.ide.config.simple.wizards;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+public class MuleConfigWizardPage extends WizardPage {
+	private Text containerText;
+	private Text fileText;
+	private ISelection selection;
+
+	public MuleConfigWizardPage(ISelection selection) {
+		super("wizardPage");
+		setTitle("Mule Configuration File");
+		setDescription("This wizard creates a new Mule configuration file with the selected namespaces.");
+		this.selection = selection;
+	}
+
+	public void createControl(Composite parent) {
+		Composite parentContainer = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		parentContainer.setLayout(layout);
+		layout.numColumns = 1;
+		layout.verticalSpacing = 9;
+		
+		createNewFileGroup(parentContainer);
+		createMuleArtifactSelector(parentContainer);
+
+		initialize();
+		dialogChanged();
+		setControl(parentContainer);
+	}
+	
+	private void createNewFileGroup(Composite parent) {
+		Group group = new Group(parent, SWT.NONE);
+		group.setText("Mule &Configuration");
+		
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		group.setLayout(layout);
+		
+		GridData gridData = new GridData();
+		gridData.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		gridData.verticalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		group.setLayoutData(gridData);		
+		
+		Label label = new Label(group, SWT.NULL);
+		label.setText("&Folder:");
+
+		containerText = new Text(group, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		containerText.setLayoutData(gd);
+		containerText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button button = new Button(group, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
+		
+		label = new Label(group, SWT.NULL);
+		label.setText("F&ile name:");
+
+		fileText = new Text(group, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		fileText.setLayoutData(gd);
+		fileText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+	}
+
+	private void createMuleArtifactSelector(Composite parent) {
+		Group group = new Group(parent, SWT.NONE);
+		group.setText("Mule &Modules / Transports:");
+
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		group.setLayout(gridLayout);
+
+		GridData gridData = new GridData();
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		gridData.verticalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		group.setLayoutData(gridData);
+
+		Table table = new Table(group, SWT.CHECK | SWT.BORDER);
+		table.setHeaderVisible(false);
+		
+		gridData = new GridData();
+		gridData.verticalSpan = 3;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.verticalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		gridData.horizontalAlignment = org.eclipse.swt.layout.GridData.FILL;
+		table.setLayoutData(gridData);
+		table.setLinesVisible(false);
+
+		table.clearAll();
+		
+		for (int i = 0; i < 40; i++) {
+			TableItem ti = new TableItem(table, SWT.CHECK);
+			ti.setChecked(false);
+			ti.setText("Axis Transport");
+		}
+	}
+
+	/**
+	 * Tests if the current workbench selection is a suitable container to use.
+	 */
+	private void initialize() {
+		if (selection != null && selection.isEmpty() == false
+				&& selection instanceof IStructuredSelection) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1)
+				return;
+			Object obj = ssel.getFirstElement();
+			if (obj instanceof IResource) {
+				IContainer container;
+				if (obj instanceof IContainer)
+					container = (IContainer) obj;
+				else
+					container = ((IResource) obj).getParent();
+				containerText.setText(container.getFullPath().toString());
+			}
+		}
+		fileText.setText("mule-config.xml");
+	}
+
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+	private void handleBrowse() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select new file container");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				containerText.setText(((Path) result[0]).toString());
+			}
+		}
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+	private void dialogChanged() {
+		IResource container = ResourcesPlugin.getWorkspace().getRoot().findMember(new Path(getContainerName()));
+		String fileName = getFileName();
+
+		if (getContainerName().length() == 0) {
+			updateStatus("File container must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+			updateStatus("File container must exist");
+			return;
+		}
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
+			return;
+		}
+		if (fileName.length() == 0) {
+			updateStatus("File name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("File name must be valid");
+			return;
+		}
+		int dotLoc = fileName.lastIndexOf('.');
+		if (dotLoc != -1) {
+			String ext = fileName.substring(dotLoc + 1);
+			if (ext.equalsIgnoreCase("xml") == false) {
+				updateStatus("File extension must be \"xml\"");
+				return;
+			}
+		}
+		updateStatus(null);
+	}
+
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public String getContainerName() {
+		return containerText.getText();
+	}
+
+	public String getFileName() {
+		return fileText.getText();
+	}
+}

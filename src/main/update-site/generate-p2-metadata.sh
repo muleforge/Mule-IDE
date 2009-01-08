48d0df69572e89ecd7155d71a57dff795bf445c5
dirk <dirk@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2009-01-08 11:27:32 +0000

    update the repository URL

--- src/main/update-site/generate-p2-metadata.sh
+++ src/main/update-site/generate-p2-metadata.sh
@@ -4,23 +4,21 @@
 #
 # make sure you have the "eclipse" command on the PATH before running this script
 
-DEPLOY_URL="http://snapshots.dist.muleforge.org/mule-ide/updates-2.0-M1"
-
 if [ ! -d plugins ]; then
-	echo "please build the update site first"
-	exit 1
+    echo "please build the update site first"
+    exit 1
 fi
 
 rm artifacts.xml content.xml
 
 eclipse -application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
-	-updateSite `pwd` \
-	-site file://`pwd`/site.xml \
-	-metadataRepository $DEPLOY_URL \
-	-metadataRepositoryName "Mule IDE Update Site" \
-	-artifactRepository $DEPLOY_URL \
-	-artifactRepositoryName "Mule IDE Artifacts" \
-	-compress \
-	-reusePack200Files \
-	-noDefaultIUs \
-	-vmargs -Xmx256m
+    -updateSite `pwd` \
+    -site file://`pwd`/site.xml \
+    -metadataRepository `pwd`/ \
+    -metadataRepositoryName "Mule IDE Update Site" \
+    -artifactRepository `pwd`/ \
+    -artifactRepositoryName "Mule IDE Artifacts" \
+    -compress \
+    -reusePack200Files \
+    -noDefaultIUs \
+    -vmargs -Xmx256m

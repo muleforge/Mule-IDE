4985de817a743542c7b9032a1e1f1cc4527be968
dirk <dirk@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2009-07-10 07:38:45 +0000

    Apply proper CPAL license header

--- src/main/plugins/org.mule.ide.debug/src/org/mule/ide/debug/DebugPlugin.java
+++ src/main/plugins/org.mule.ide.debug/src/org/mule/ide/debug/DebugPlugin.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.ide.debug;
 
 import org.eclipse.core.runtime.Plugin;
@@ -8,43 +18,27 @@ import org.osgi.framework.BundleContext;
  */
 public class DebugPlugin extends Plugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "org.mule.ide.debug";
 
-	// The shared instance
 	private static DebugPlugin plugin;
 	
-	/**
-	 * The constructor
-	 */
 	public DebugPlugin() {
+		super();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static DebugPlugin getDefault() {
 		return plugin;
 	}
-
 }

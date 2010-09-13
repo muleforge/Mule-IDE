e6d653db98e9b6aa42913110ce214632c8240af0
dirk <dirk@4cf43e26-4b37-0410-a4ee-9489ae3e24fc>
2010-09-13 09:37:22 +0000

    MULEIDE-185 (Mule 3.x bookstore example needs to download extra Jetty libraries to work)

--- src/main/plugins/org.mule.ide.project/src/org/mule/ide/project/internal/runtime/samples/MuleSampleFactory.java
+++ src/main/plugins/org.mule.ide.project/src/org/mule/ide/project/internal/runtime/samples/MuleSampleFactory.java
@@ -29,6 +29,7 @@ public class MuleSampleFactory
     static
     {
         Factories = new HashMap<String, IMuleSampleFactory>();
+        Factories.put("mule-example-bookstore", new BookstoreExampleFactory());
         Factories.put("mule-example-errorhandler", new ErrorhandlerExampleFactory());
         Factories.put("mule-example-scripting", new ScriptingExampleFactory());
     }
@@ -49,6 +50,34 @@ public class MuleSampleFactory
     {
         IMuleSampleProject createSampleProject(String artifactid, String name,
            String description, File dir);
+    }
+    
+    private static class BookstoreExampleFactory implements IMuleSampleFactory
+    {
+        private static final String JETTY_JSP_DOWNLOAD_URL = "http://repo1.maven.org/maven2/org/mortbay/jetty/jsp-2.1-jetty/6.1.20/jsp-2.1-jetty-6.1.20.jar";
+        private static final String JSP_GLASSFISH_DOWNLOAD_URL = "http://repo1.maven.org/maven2/org/mortbay/jetty/jsp-2.1-glassfish/9.1.1.B60.25.p2/jsp-2.1-glassfish-9.1.1.B60.25.p2.jar";
+        private static final String JSP_API_DOWNLOAD_URL = "http://repo1.maven.org/maven2/org/mortbay/jetty/jsp-api-2.1-glassfish/9.1.1.B60.25.p2/jsp-api-2.1-glassfish-9.1.1.B60.25.p2.jar";
+        private static final String JDT_CORE_DOWNLOAD_URL = "http://repo2.maven.org/maven2/org/eclipse/jdt/core/3.1.1/core-3.1.1.jar";
+        
+        public IMuleSampleProject createSampleProject(String artifactId, String name, 
+            String description, File dir)
+        {
+            MuleSampleProject sample = new MuleSampleProject(artifactId, name, description, dir);
+
+            URL jettyJspSupport = UrlUtils.urlFromString(JETTY_JSP_DOWNLOAD_URL);
+            sample.addLibraryDownloadedFrom(jettyJspSupport);
+
+            URL jspGlassfish = UrlUtils.urlFromString(JSP_GLASSFISH_DOWNLOAD_URL);
+            sample.addLibraryDownloadedFrom(jspGlassfish);
+
+            URL jspApi= UrlUtils.urlFromString(JSP_API_DOWNLOAD_URL);
+            sample.addLibraryDownloadedFrom(jspApi);
+
+            URL jdtCore = UrlUtils.urlFromString(JDT_CORE_DOWNLOAD_URL);
+            sample.addLibraryDownloadedFrom(jdtCore);
+
+            return sample;
+        }
     }
     
     private static class ErrorhandlerExampleFactory implements IMuleSampleFactory
